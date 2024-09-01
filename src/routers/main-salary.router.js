5c8dd9edd176299dade9b6288f3446ea7010e276
fahmiitmamul <itmamul2004@gmail.com>
2024-09-01 17:09:04 +0700

    fix: get employee id instead id

--- src/routers/main-salary.router.js
+++ src/routers/main-salary.router.js
@@ -3,7 +3,7 @@ const mainSalaryController = require('../controllers/main-salary.controller')
 
 mainSalaryRouter.get('/', mainSalaryController.getAll)
 mainSalaryRouter.get('/:id', mainSalaryController.findOneById)
-mainSalaryRouter.get('/employee/:id', mainSalaryController.findOneById)
+mainSalaryRouter.get('/employee/:id', mainSalaryController.findOneByEmployeeId)
 mainSalaryRouter.post('/', mainSalaryController.create)
 mainSalaryRouter.patch('/:id', mainSalaryController.update)
 mainSalaryRouter.delete('/:id', mainSalaryController.delete)
